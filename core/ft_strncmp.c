9ae6de4450677f5e8631adc3e0d3e9bdce285edd
Denny Cerritos portillo <dcerrito@c3r9s4.42madrid.com>
2022-03-26 19:34:40 +0100

    feat(ft_strncmp.c): adds portable function definition

--- core/ft_strncmp.c
+++ core/ft_strncmp.c
@@ -1,28 +1,26 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/03/26 19:14:33 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/03/26 19:34:05 by dcerrito         ###   ########.fr       */
+/*   Created: 2022/03/26 19:31:06 by dcerrito          #+#    #+#             */
+/*   Updated: 2022/03/26 19:33:36 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stddef.h>
 
-int	ft_memcmp(const void *__s1, const void *__s2, size_t n)
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	const char	*s1;
-	const char	*s2;
-	size_t		i;
+	while (n-- && *s1)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
 
-	i = -1;
-	s1 = __s1;
-	s2 = __s2;
-	while (++i, i < n)
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
 	return (0);
 }
