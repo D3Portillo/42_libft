f09d585db14faf8ed86741b30fcac6e7f9905137
Denny Cerritos portillo <dcerrito@c2r17s3.42madrid.com>
2022-03-28 02:07:04 +0200

    fix(ft_strchr.c): type convert __c to char

--- core/ft_strchr.c
+++ core/ft_strchr.c
@@ -6,12 +6,15 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 19:46:39 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/03/26 19:49:47 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/03/28 01:13:15 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strchr(const char *s, int c)
+char	*ft_strchr(const char *s, int __c)
 {
+	char	c;
+
+	c = __c;
 	while (*s || c == 0)
 	{
 		if (*s == c)
