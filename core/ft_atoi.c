a7e35e8fc20ee33b1aca482cdd8f4a2114d6c0d1
Denny Cerritos portillo <dcerrito@c2r11s4.42madrid.com>
2022-03-29 02:36:46 +0200

    fix(ft_atoi.c): better handle MIN MAX for LONG values

--- core/ft_atoi.c
+++ core/ft_atoi.c
@@ -1,8 +1,16 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/28 04:45:33 by dcerrito          #+#    #+#             */
+/*   Updated: 2022/03/29 02:30:02 by dcerrito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-
-#include <limits.h>
-
-int	ft_isdigit(int c);
+#include "libft.h"
 
 static int	is_space(char c)
 {
@@ -17,9 +25,11 @@ int	ft_atoi(const char *str)
 {
 	long long int	result;
 	int				multiplier;
+	int				i;
 
 	multiplier = 1;
 	result = 0;
+	i = 0;
 	while (is_space(*str))
 		++str;
 	if (*str == '-')
@@ -28,13 +38,13 @@ int	ft_atoi(const char *str)
 		++str;
 	while (ft_isdigit(*str))
 	{
-		result = (result * 10) + (*str++ - '0');
-		if ((result - LONG_MAX) > 0)
+		if (++i >= 18)
 		{
-			if (multiplier)
+			if (multiplier == 1)
 				return (-1);
 			return (0);
 		}
+		result = (result * 10) + (*str++ - '0');
 	}
 	return (result * multiplier);
 }
