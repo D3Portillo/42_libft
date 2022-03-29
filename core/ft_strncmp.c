7022a02ae933d25cf74ef5b2126a609d98b41f90
Denny Cerritos portillo <dcerrito@c2r11s4.42madrid.com>
2022-03-29 02:39:06 +0200

    fix(ft_strncmp.c): better cast expected unsigned char typing and handle null strings

--- core/ft_strncmp.c
+++ core/ft_strncmp.c
@@ -6,21 +6,25 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 19:31:06 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/03/26 19:33:36 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/03/29 01:05:23 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
-int	ft_strncmp(const char *s1, const char *s2, size_t n)
+int	ft_strncmp(const char *__s1, const char *__s2, size_t n)
 {
-	while (n-- && *s1)
+	unsigned char	*s1;
+	unsigned char	*s2;
+
+	s1 = (void *)__s1;
+	s2 = (void *)__s2;
+	while (n-- && (*s1 || *s2))
 	{
 		if (*s1 != *s2)
 			return (*s1 - *s2);
 		s1++;
 		s2++;
 	}
-
 	return (0);
 }
