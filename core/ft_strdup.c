0eba0c4febdd0e95b6579aef700fba3a21104944
Denny Cerritos portillo <dcerrito@c2r11s4.42madrid.com>
2022-03-29 02:45:23 +0200

    fix(ft_strdup.c): enclose latest element at buffer with \0

--- core/ft_strdup.c
+++ core/ft_strdup.c
@@ -6,26 +6,22 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 02:04:24 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/03/28 02:04:42 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/03/28 08:33:28 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <stddef.h>
-
-void	*ft_memcpy(void *__dst, const void *__src, size_t n);
-int		ft_strlen(char *str);
+#include "libft.h"
 
 char	*ft_strdup(const char *source)
 {
 	int		size;
 	char	*dest;
 
-	size = ft_strlen(source);
+	size = ft_strlen((char *)source);
 	dest = malloc(size * sizeof(char) + 1);
 	if (!dest)
 		return (0);
 	ft_memcpy(dest, source, size * sizeof(char));
-	dest[size - 1] = 0;
+	dest[size] = 0;
 	return (dest);
 }
