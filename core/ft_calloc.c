ddbab7d80b5240dec66e27b511e807985e2aadd2
Denny Cerritos portillo <dcerrito@c2r11s4.42madrid.com>
2022-03-29 02:44:38 +0200

    feat(ft_callo.c): better handle if malloc can allocate the expected mem size

--- core/ft_calloc.c
+++ core/ft_calloc.c
@@ -6,23 +6,21 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 02:05:25 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/03/28 02:05:26 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/03/28 08:31:03 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-
-void	*ft_memset(void *__dest, int c, size_t len);
+#include "libft.h"
 
 void	*ft_calloc(size_t count, size_t size)
 {
 	void	*alloc;
 	size_t	bytes;
 
-	bytes = (count * size) + 1;
-	if (bytes < 1)
-		return (0);
+	bytes = (count * size);
 	alloc = malloc(bytes);
+	if (alloc == 0)
+		return (0);
 	ft_memset(alloc, 0, bytes);
 	return (alloc);
 }
