0ddb58880dc3c43ad18df0bbea23c06b794aa122
Denny Cerritos portillo <dcerrito@c2r11s4.42madrid.com>
2022-03-29 02:40:11 +0200

    fix(ft_strnstr.c): do not look after len elements reached at needle searching

--- core/ft_strnstr.c
+++ core/ft_strnstr.c
@@ -6,24 +6,28 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/26 19:14:33 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/03/28 01:08:03 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/03/29 01:44:41 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stddef.h>
+#include "libft.h"
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int	i;
+	int		i;
+	size_t	limit;
 
-	while (*haystack)
+	i = 0;
+	limit = 0;
+	while (limit <= len)
 	{
 		i = 0;
-		while (needle[i] && needle[i] == haystack[i])
+		while (limit + i < len && needle[i] && needle[i] == haystack[i])
 			++i;
-		if (needle[i] == 0)
-			return (haystack);
+		if (needle[i] == 0 || len == 0)
+			return ((char *)haystack);
 		haystack++;
+		limit++;
 	}
 	return (0);
 }
