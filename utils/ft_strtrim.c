090199fb7c2dafe830f9af994d96c7dc90282e5e
Denny Cerritos portillo <dcerrito@c1r17s4.42madrid.com>
2022-04-07 10:29:31 +0200

    feat(ft_strtrim.c): add function implementation

--- utils/ft_strtrim.c
+++ utils/ft_strtrim.c
@@ -6,14 +6,24 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 06:40:28 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/03/31 06:49:51 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/04/05 21:49:17 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_strtrim(char const *s1, char const *set)
-{
-	char	*a;
+#include "libft.h"
 
-	a = (char *)set;
-	return ((char *)s1);
+char	*ft_strtrim(char const *__src, char const *set)
+{
+	char	*src;
+	int		latest_idx;
+
+	if (!__src || !set)
+		return (NULL);
+	src = (char *)__src;
+	while (*src && ft_strchr(set, *src))
+		src++;
+	latest_idx = ft_strlen(src);
+	while (latest_idx && ft_strchr(set, src[latest_idx]))
+		latest_idx--;
+	return (ft_substr(src, 0, latest_idx + 1));
 }
