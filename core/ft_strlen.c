19bff912821182785cd4914893ec5bd1f0f82aeb
Denny Cerritos portillo <dcerrito@c3r14s4.42madrid.com>
2022-04-07 22:51:19 +0200

    fix(ft_strlen.c): force segfault with str=NULL

--- core/ft_strlen.c
+++ core/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/23 03:33:01 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/04/07 21:35:36 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/04/07 22:06:22 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_strlen(const char *str)
 	int	size;
 
 	size = 0;
-	while (str && str[size])
+	while (str[size])
 		++size;
 	return (size);
 }
