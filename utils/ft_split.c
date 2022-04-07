6977a604b0cfb9b91b169f1c168f07e069b99557
Denny Cerritos portillo <dcerrito@c3r14s4.42madrid.com>
2022-04-07 21:58:23 +0200

    feat(ft_split.c): better malloc error handling

--- utils/ft_split.c
+++ utils/ft_split.c
@@ -6,7 +6,7 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 03:09:16 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/04/07 18:56:26 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/04/07 21:30:58 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ static int	buffer_size(char *str, char delimiter, int i)
 			++i;
 		++str;
 	}
-	return (i + 1);
+	return (i);
 }
 
 static int	set_zero(int *n)
@@ -36,6 +36,17 @@ static void	*set_buffer(char **buffer, char *content)
 	return (*buffer = content);
 }
 
+static int	freed_content(char **content, int elems, int free_content)
+{
+	if (free_content)
+	{
+		while (elems--)
+			free(content[elems]);
+		free(content);
+	}
+	return (free_content);
+}
+
 char	**ft_split(char const *__str, char delimiter)
 {
 	char	**result;
@@ -46,13 +57,17 @@ char	**ft_split(char const *__str, char delimiter)
 
 	if (!set_buffer(&str, (char *)__str))
 		return (NULL);
-	result = malloc(sizeof(char *) * buffer_size((head = str), delimiter, 0));
+	result = malloc(sizeof(char *) * buffer_size((head = str), delimiter, 1));
 	if (set_zero(&elems), set_zero(&i), !result)
 		return (NULL);
 	while (*str)
 	{
 		if (*str != delimiter && (str[1] == delimiter || !str[1]))
+		{
 			result[elems++] = ft_substr(&str[-i], 0, set_zero(&i) + 1);
+			if (freed_content(result, elems - 1, !result[elems - 1]))
+				return (NULL);
+		}
 		else if (*str != delimiter)
 			++i;
 		++str;
