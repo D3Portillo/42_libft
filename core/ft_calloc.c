330ee82881f89dce0f85dc3c43694bc7b7f3ac1d
Denny Cerritos portillo <dcerrito@c1r17s4.42madrid.com>
2022-04-07 19:38:42 +0200

    fix(ft_calloc.c): handle if size > SIZE_MAX

--- core/ft_calloc.c
+++ core/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/28 02:05:25 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/04/03 20:39:32 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/04/07 19:32:34 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,10 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*alloc;
 	size_t	bytes;
 
-	bytes = count * size;
-	alloc = malloc(bytes);
-	if (alloc == 0)
+	if (size >= SIZE_MAX)
+		return (NULL);
+	alloc = malloc((bytes = count * size));
+	if (!alloc)
 		return (NULL);
 	ft_memset(alloc, 0, bytes);
 	return (alloc);
