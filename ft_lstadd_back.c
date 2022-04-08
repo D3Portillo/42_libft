89de0730f083c9c1d255524aca869a733fe41e83
Denny Cerritos portillo <dcerrito@c2r1s3.42madrid.com>
2022-04-08 20:42:54 +0200

    refactor: move bonus/* to ROOT

---
+++ ft_lstadd_back.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/03/31 20:38:21 by dcerrito          #+#    #+#             */
+/*   Updated: 2022/03/31 23:54:45 by dcerrito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	if (*lst == NULL)
+		*lst = new;
+	else
+		ft_lstlast(*lst)->next = new;
+}
