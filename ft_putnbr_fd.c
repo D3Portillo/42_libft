39caf9f7c175cf2d0f50663b396863de74189c0f
Denny Cerritos portillo <dcerrito@c2r1s3.42madrid.com>
2022-04-08 20:48:30 +0200

    refactor: move utils/* to ROOT

---
+++ ft_putnbr_fd.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_fd.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 03:09:08 by dcerrito          #+#    #+#             */
+/*   Updated: 2022/04/07 21:31:26 by dcerrito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	appends(long int n, int fd)
+{
+	if (n < 0 && write(fd, "-", 1))
+		n *= -1;
+	if (n < 10)
+		return (ft_putchar_fd(n + '0', fd));
+	return (appends(n / 10, fd), appends(n % 10, fd));
+}
+
+void	ft_putnbr_fd(int n, int fd)
+{
+	if (fd > 0)
+		appends(n, fd);
+}
---
+++ ft_split.c
@@ -0,0 +1,68 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_split.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/02 03:09:16 by dcerrito          #+#    #+#             */
+/*   Updated: 2022/04/08 15:51:34 by dcerrito         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+static int	buffer_size(char *str, char delimiter, int i)
+{
+	while (*str)
+	{
+		if (*str != delimiter && (str[1] == delimiter || !str[1]))
+			++i;
+		++str;
+	}
+	return (i);
+}
+
+static int	set_zero(int *n)
+{
+	int	init_state;
+
+	return (init_state = *n, *n = 0, init_state);
+}
+
+static int	freed_content(char **content, int elems, int free_content)
+{
+	if (free_content)
+		while (elems--)
+			free(content[elems]);
+	return (free_content);
+}
+
+char	**ft_split(char const *__str, char delimiter)
+{
+	char	**result;
+	char	*str;
+	char	*head;
+	int		elems;
+	int		i;
+
+	str = (char *)__str;
+	if (!str)
+		return (NULL);
+	result = malloc(sizeof(char *) * buffer_size((head = str), delimiter, 1));
+	if (set_zero(&elems), set_zero(&i), !result)
+		return (NULL);
+	while (*str)
+	{
+		if (*str != delimiter && (str[1] == delimiter || !str[1]))
+		{
+			result[elems++] = ft_substr(&str[-i], 0, set_zero(&i) + 1);
+			if (freed_content(result, elems - 1, !result[elems - 1]))
+				return (free(result), NULL);
+		}
+		else if (*str != delimiter)
+			++i;
+		++str;
+	}
+	return ((result[elems] = NULL), result);
+}
