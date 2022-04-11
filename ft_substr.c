b6fc22cc013904ba70e594629a0db35f730dce6c
Denny Cerritos portillo <dcerrito@c3r13s5.42madrid.com>
2022-04-11 16:21:30 +0200

    fix(ft_substr.c): finish function execution if start index greater than string size

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: dcerrito <dcerrito@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/02 03:09:32 by dcerrito          #+#    #+#             */
-/*   Updated: 2022/04/06 04:22:38 by dcerrito         ###   ########.fr       */
+/*   Updated: 2022/04/11 04:01:09 by dcerrito         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,20 @@
 
 char	*ft_substr(char const *src, unsigned int start, size_t size)
 {
-	char			*result;
-	unsigned int	size_to_create;
+	char	*result;
+	size_t	size_to_create;
+	size_t	str_len;
 
 	if (!src)
 		return (NULL);
-	size_to_create = ft_strlen((char *)src);
+	str_len = ft_strlen((char *)src);
+	size_to_create = (str_len);
 	if (size < size_to_create)
 		size_to_create = size;
 	result = ft_calloc(size_to_create + 1, sizeof(char));
 	if (!result)
 		return (NULL);
-	if (start > size || start > size_to_create)
+	if (start > str_len)
 		return (result);
 	ft_memcpy(result, &src[start], size_to_create);
 	return (result);
